e070a019d63b95c843d87d51e0cbd989cdc3d06b
jn_xyp <jinan.xyp@gmail.com>
2019-08-05 14:43:05 +0800

    为云对象模型添加path属性，原path计算方法改名为_path； 添加文件名验证器； 修改云对象的validate_unique方法，以验证其唯一性；

--- DiurenCloud/models.py
+++ DiurenCloud/models.py
@@ -1,11 +1,13 @@
 from django.conf.global_settings import MEDIA_ROOT
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.core.files.storage import default_storage, Storage
 from django.db import models
 from django.utils.translation import gettext, gettext_lazy as _
 
 # Create your models here.
 from DiurenCloud.apps import USER_UPLOAD_PATH
+from DiurenCloud.validators import validate_object_name_special_characters
 
 
 class CloudUserProfile(models.Model):
@@ -27,15 +29,38 @@ class CloudUserProfile(models.Model):
         profile.root_directory.get_object_by_path('/'.join(path_split[1:]))
 
 
-# todo 添加文件名校验 Validator
+# 云对象分为文件和文件夹两类，同目录下对象不允许重名。
 class CloudObjectBase(models.Model):
     class Meta:
         abstract = True
 
-    name = models.CharField(max_length=128)
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, validators=[validate_object_name_special_characters])
+    owner = models.ForeignKey(verbose_name=_('所有者'), to=User, on_delete=models.CASCADE)
 
     date_updated = models.DateTimeField(verbose_name=_('修改时间'), auto_created=True, auto_now=True)
+    # 文件夹的路径必须在结尾添加'/'
+    path = models.CharField(verbose_name=_('路径'), max_length=256, auto_created=True)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.path = self._path
+        return super().save(force_insert, force_update, using, update_fields)
+
+    def validate_unique(self, exclude=None):
+        val = super().validate_unique(exclude)
+        dirs = [d.name for d in self.parent.directories]
+        files = [f.name for f in self.parent.files]
+        if self.name in dirs or self.name in files:
+            raise ValidationError(_('对象名称重复！'), code='duplicate_object_name')
+        return val
+
+    @property
+    def _path(self):
+        raise NotImplementedError()
+
+    @classmethod
+    def get_object_by_path(cls, path: str):
+        cls.objects.get(path=path)
 
 
 class CloudDirectory(CloudObjectBase):
@@ -49,7 +74,7 @@ class CloudDirectory(CloudObjectBase):
         super().__init__()
         self._meta.get_field('name').verbose_name = _('文件夹名')
 
-    def get_object_by_path(self, path: str):
+    def get_object_by_path_recursive(self, path: str):
         if '/' not in path:  # 如果是一个文件名
             return self.files.get(name=path)
         else:
@@ -61,9 +86,9 @@ class CloudDirectory(CloudObjectBase):
                 return dir.get_object_by_path('/'.join(path_split[1:]))
 
     @property
-    def path(self):
+    def _path(self):
         if self.parent:
-            p = self.parent.path
+            p = self.parent._path
         else:
             p = self.owner.cloud_profile.user_upload_root
         return p + self.name + '/'
@@ -82,9 +107,9 @@ class CloudFile(CloudObjectBase):
         self._meta.get_field('name').verbose_name = _('文件名')
 
     @property
-    def path(self):
+    def _path(self):
         if self.parent:
-            p = self.parent.path
+            p = self.parent._path
         else:
             p = self.owner.cloud_profile.user_upload_root
         return p + self.name
